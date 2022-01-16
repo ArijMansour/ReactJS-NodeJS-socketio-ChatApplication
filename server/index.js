c57d9a3bb98aa712d2bd7765c93cc49563ba3d3e
unknown <arijbenmansour1@gmail.com>
2022-01-17 00:08:55 +0100

    Socket io connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express =require('express');
 const socketio=require('socket.io');
 const http = require('http');
-
+const router = require('./router');
 const app=express () ;
 
 // socket.io setup 
 const PORT =process.env.PORT || 5000
 const server= http.createServer(app);
-const io = socketio(server);
+const io = socketio(server); 
+// integrate socket io 
+
+//socket connection 
+io.on('connection', (socket)=> {
+ console.log('we have a new connection!!') ;
+ socket.on('disconnect',() =>{
+     console.log('user had left !! ')
+ })
+});
+app.use(router) ;
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`)); // altgr 7 
 
+ 
