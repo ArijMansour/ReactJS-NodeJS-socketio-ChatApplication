5d64fa3140638562cb5269010ca9e848ce60b6ae
unknown <arijbenmansour1@gmail.com>
2022-01-17 22:11:17 +0100

    socket connection

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ io.on('connection', (socket)=> {
  const {error,  user}=addUser({id:socket.id,name,room}) ; 
  if(error) return callback(error); 
 
+ socket.emit('message',{user:'admin',text:`${user.name},Welcome to the room ${user.room}`})
+ socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name},has joind`})
+
  socket.join(user.room) ;
+
+ callback() ; 
     /* console.log(name,room) ;
      const error= true ; 
 if(error){
@@ -26,6 +31,14 @@ if(error){
 }*/
     //Error handling
  })
+ socket.on('sendMessage',(message,callback)=>{
+     const user= getUser(socket.id);
+     io.to(user.room).emit('message', {user:user.name,text:message}) ; 
+     callback() ;
+
+ })
+
+
  socket.on('disconnect',() =>{
      console.log('user had left !! ')
  })
