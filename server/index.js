5756858fb223ffe22aa7144b2539b2944ace087c
unknown <arijbenmansour1@gmail.com>
2022-01-17 01:41:58 +0100

    Socket io backend setup

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const socketio=require('socket.io');
 const http = require('http');
 const router = require('./router');
 const app=express () ;
-
+const {addUser,removeUser,getUsersInRoom,getUser} = require ('./users.js') ;
 // socket.io setup 
 const PORT =process.env.PORT || 5000
 const server= http.createServer(app);
@@ -13,6 +13,19 @@ const io = socketio(server);
 //socket connection 
 io.on('connection', (socket)=> {
  console.log('we have a new connection!!') ;
+ socket.on('join', ({name,room}, callback)=>{
+
+ const {error,  user}=addUser({id:socket.id,name,room}) ; 
+ if(error) return callback(error); 
+
+ socket.join(user.room) ;
+    /* console.log(name,room) ;
+     const error= true ; 
+if(error){
+    callback({error:'error'}); 
+}*/
+    //Error handling
+ })
  socket.on('disconnect',() =>{
      console.log('user had left !! ')
  })
